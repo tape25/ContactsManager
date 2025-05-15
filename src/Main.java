952c28264bfe124c78f64825c22c3e6c39de751e
BAKAYOKO TOKPA MOHAMED <bakayokotokpamohamed@gmail.com>
2025-05-15 17:23:51 +0000

    Exercice ContactsManager

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,26 @@ public class Main {
         ContactsManager  myContactsManager = new ContactsManager();
 
         Contact contact = new Contact();
-        contact.name = "Soeurette";
-        contact.phoneNumber = "0202074486";
+        contact.name = "Tape Sopie";
+        contact.phoneNumber = "0759665896";
         myContactsManager.addContact(contact);
 
         Contact contact1 = new Contact();
-        contact1.name = "Melanie";
-        contact1.phoneNumber = "0101434567";
+        contact1.name = "Ornella";
+        contact1.phoneNumber = "0103405268";
         myContactsManager.addContact(contact1);
 
         Contact contact2 = new Contact();
-        contact2.name = "Patrick ";
-        contact2.phoneNumber = "0707654589";
+        contact2.name = "Natoutou";
+        contact2.phoneNumber = "0707988337";
         myContactsManager.addContact(contact2);
 
         Contact contact3 = new Contact();
-        contact3.name = "grace";
-        contact3.phoneNumber = "0239239499";
+        contact3.name = "Hermine";
+        contact3.phoneNumber = "0595730524";
         myContactsManager.addContact(contact3);
 
-        Contact phone = myContactsManager.searchContact("Soeurette");
+        Contact phone = myContactsManager.searchContact("Hermine");
 
         System.out.println(phone.phoneNumber);
     }
